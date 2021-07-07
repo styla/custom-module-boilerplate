54ea63d71199614af5218555f42ef97250c3b48f
Antonio Cosentino <hello@antoniocosentino.it>
2021-07-07 19:00:54 +0200

    added dynamic behavior to cart overlay

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,19 @@ window.styla.registerCustomModule('NAME', async (wrapper, content) => {
     // console.log('wrapper', wrapper);
     // console.log('content', content);
 
+    const cartContainer = wrapper.querySelector('.cartContainer');
+    
+    const onCartClick = () => {
+        const cartOverlay = wrapper.querySelector<HTMLDivElement>('.cartOverlay');
+
+        const currentOverlayState = cartOverlay.dataset.overlayVisible;
+        
+        if (currentOverlayState == 'yes') {
+            cartOverlay.dataset.overlayVisible = 'no';
+        } else {
+            cartOverlay.dataset.overlayVisible = 'yes';
+        }
+    }
+    
+    cartContainer.addEventListener( 'click', onCartClick  );
 });
