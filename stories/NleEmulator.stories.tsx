74d4a574613369f9ef8f258a77718ec83fddf669
Antonio Cosentino <hello@antoniocosentino.it>
2021-07-07 12:27:49 +0200

    basic schema conversion to storybook controls

--- stories/NleEmulator.stories.tsx
+++ stories/NleEmulator.stories.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
 
 import { NleEmulator } from './NleEmulator';
+import * as schema from '../src/schema.json';
 
 export default {
-  title: 'Example/NleEmulator',
-  component: NleEmulator,
+    title: 'Example/NleEmulator',
+    component: NleEmulator,
 };
 
 const Template = (args) => {
-  console.log('args', args);
-  return (<NleEmulator {...args} />);
+    return (<NleEmulator {...args} />);
 };
 
-const convertSchema = (schema: any) => {
-  // todo convert schema to props
-  return {
-    primary: true,
-  };
+const getConvertedProp = ( propertyKey, property ) => {
+    const { type } = property;
+
+    switch ( type ) {
+        case 'string':
+        default:
+            return {
+                [propertyKey]: `insert ${ propertyKey } text here`
+            }
+
+        case 'number':
+            return {
+                [propertyKey]: property.default
+            }
+
+    }
+}
+
+const convertSchema = () => {
+    
+    const allControls = {}
+
+    const contentControls = {};
+    
+    for ( const [key, value] of Object.entries( schema.content?.data?.properties ) ) {
+        const newObj = getConvertedProp( key, value );
+        Object.assign( contentControls, newObj );
+    }
+    
+    Object.assign( allControls, contentControls );
+
+    const settingsControls = {};
+    
+    for ( const [key, value] of Object.entries( schema.settings?.data?.properties ) ) {
+        const newObj = getConvertedProp( key, value );
+        Object.assign( settingsControls, newObj );
+    }
+    
+    Object.assign( allControls, contentControls, settingsControls );
+
+    
+    return allControls;
 }
 
 export const Render = Template.bind({});
-Render.args = convertSchema({});
+Render.args = convertSchema();
