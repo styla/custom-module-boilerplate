801af2677ae9a53f0f52fa970b6f540d25348a20
ssachtleben <ssachtleben@gmail.com>
2021-07-07 17:51:01 +0200

    use plain handlebars for compilation

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,10 +21,27 @@ module.exports = {
     }
   },
   webpackFinal: async (sbWebpackConfig) => {
+    /**
     sbWebpackConfig.module.rules.push({
       test: /\.handlebars$/,
       use: ['handlebars-loader'],
       include: path.resolve(__dirname, '../src'),
+    });
+    */
+
+    // TODO: this fails horribly
+    sbWebpackConfig.module.rules.push({
+      test: /\.handlebars$/,
+      use: ['raw-loader'],
+      include: path.resolve(__dirname, '../src'),
+    });
+
+    sbWebpackConfig.resolve = Object.assign({}, sbWebpackConfig.resolve, {
+      fallback: {
+        fs: false,
+        path: require.resolve('path-browserify'),
+        readline: false
+      }
     });
 
     return sbWebpackConfig;
--- stories/NleEmulator.tsx
+++ stories/NleEmulator.tsx
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
+import * as handlebars from 'handlebars';
+//import helpers from 'handlebars-helpers';
 
 // Import custom module source
 import '../src/index';
@@ -8,6 +10,13 @@ import schema from '../src/schema';
 
 // @ts-ignore
 import template from '../src/template.handlebars';
+
+/**
+// add 188 handlebars helpers https://www.npmjs.com/package/handlebars-helpers
+helpers({
+    handlebars: handlebars
+});
+*/
 
 const pickAttributes = (obj: any, isContent: boolean) => {
     for (const propName in obj) {
@@ -31,11 +40,6 @@ export const NleEmulator = ({ ...props }) => {
         }, 100);
     }, [props]);
 
-
-
-    const content = pickAttributes(Object.assign({}, props), true);
-    const settings = pickAttributes(Object.assign({}, props), false);
-
     const exposedData = {
         content: pickAttributes(Object.assign({}, props), true),
         settings: pickAttributes(Object.assign({}, props), false),
@@ -44,11 +48,48 @@ export const NleEmulator = ({ ...props }) => {
         },
     };
 
-    console.log('exposedData', exposedData);
+    /**
+     * Currently the custom_fields are a string instead of an array,
+     * as well you can not iterate over the arrays of array structure with handlebars.
+     *
+     * Ideally in the future custom_fields would be a JSON object instead of a string, and contain
+     * object attributes, so that it can be accessed directly without a helper function.
+     *
+     * example usage:
+     * JSONL custom_fields: "[["fbt_product","11531"],["fbt_product","1296"],["usp_1","TSRGD Compliant"]]
+     * Handlebars usage:
+     * {{customField context.areaContext.products.[0].custom_fields 'attach_title_1'}}
+     */
+    handlebars.registerHelper("customFields", function (customField: string, fieldName: string) {
+        if (!customField) {
+            return
+        }
+
+        const customArray = JSON.parse(customField);
+
+        if (!Array.isArray(customArray) || customArray.length === 0) {
+            return
+        }
+
+        for (const element of customArray) {
+            if (Array.isArray(element) && element[0] === fieldName) {
+                return element[1];
+            }
+        }
+    });
+
+    /**
+     * Return the main product of the local context on a PDP
+     */
+    handlebars.registerHelper("getMainProduct", function (): any {
+        if ((exposedData.context.areaContext as any).entity?.type === "PRODUCT") {
+            return (exposedData.context.areaContext as any).products?.find((value: any) => value.id == (exposedData.context.areaContext as any).areaContext.entity.id)
+        }
+    });
 
     return (
         <Fragment>
-            <div id="custom-module" dangerouslySetInnerHTML={{ __html: template(exposedData) }}></div>
+            <div id="custom-module" dangerouslySetInnerHTML={{ __html: handlebars.compile(template)(exposedData) }}></div>
         </Fragment>
 
     );
