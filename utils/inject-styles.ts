5b854ab74644303b1f10359d5090ff38090ed072
Antonio Cosentino <hello@antoniocosentino.it>
2023-12-19 12:16:06 +0100

    ST-3882: improvements after CR

--- utils/inject-styles.ts
+++ utils/inject-styles.ts
@@ -1,11 +1,8 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 // This script is responsible for:
 // - Getting the minified CSS file (which is the result of the Sass file compile)
 // - Include this CSS in the head of the original Handlebars file
 // - Save the updated Handlebars file in the temp folder, so that it can be used during the build process
 
-// @ts-ignore
 import * as fs from 'fs';
 
 const cssFilePath = '_dev_tmp/css/styles.css';
